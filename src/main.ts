619f1dad5adc78dd5df75b1cf983713f51f14367
andrescuello7 <andrescuellotrabajo@gmail.com>
2023-01-25 05:50:00 +0200

    update port

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { AppModule } from './app.module';
 require('dotenv').config({ path: `../${process.env.NODE_ENV}.env` });
 
 async function bootstrap() {
+  let port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(3000);
-  console.log('server listening on port ', 3000);
+  await app.listen(port);
+  console.log('server listening on port ', port);
 }
 bootstrap();
